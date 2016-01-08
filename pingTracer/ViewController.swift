7af204955c3d31e4c9c62a9cfde92eba5f19b54c
ivangutnik <gilman06@gmail.com>
2016-01-08 12:35:10 +0300

    Initial Commit

---
+++ pingTracer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pingTracer
+//
+//  Created by Ivan Gutnik on 08/01/16.
+//  Copyright (c) 2016 Ivan Gutnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
